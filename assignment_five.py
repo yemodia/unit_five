58cee9ae4d452009c4821db073029604f1907427
Yerim <yerim@iMac6.local>
2018-10-15 11:19:59 -0400

    Today I added different functions and wrote some of my main function.

--- assignment_five.py
+++ assignment_five.py
@@ -1,13 +1,32 @@
 import random
 
-def user_play ():
-   want_play = input("Do you want to play?")
-   return want_play
+def user_play():
+    while True:
+        want_play = input("Do you want to play?")
+        if want_play == "y" or want_play == "n":
+            break
+    return want_play
+
+def random_number():
+    number = random.randint (1,100)
+    return number
+
 
 
 def main():
-    if want_play == y:
+    while True:
+        if user_play() == "y":
+            answer = random_number()
+        user_guess = input("Pick a number between 1 and 100:")
+        if user_guess == random_number():
+            print("You guessed it")
+            break
 
-    else
+        else:
+            print("Ok, see you later")
+
+            if user_guess <=1  and user_guess <= 100:
+                
+
 
 main()
